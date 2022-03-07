1f4033edd0f50a91a30ee2f1b249a873777ce217
Abdul Halim <32664853+abdul0001@users.noreply.github.com>
2022-03-07 20:23:56 +0100

    Update fem1d.m

--- FEM1d/fem1d.m
+++ FEM1d/fem1d.m
@@ -3,12 +3,10 @@
 %%             u(0)=0; u(1)=0;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-clear all
-for ip=1:1
-    
-N=100*ip; % Mesh points
+clear all 
+N=100; % Mesh points
 x=linspace(0,1,N+1);
-h=1/N; di(ip)=1/N;
+h=1/N; 
 for i=1:N,
     Elem(i,:)=[i i+1];
 end
@@ -23,13 +21,9 @@ fullnodes=[1:(N+1)];
 freenodes=setdiff(fullnodes,Db);
 uh=zeros(N+1,1);
 uh(freenodes)=A(freenodes,freenodes)\L(freenodes,1);
-for j=1:(N+1),
+for j=1:(N+1)
     u(j,1)=ue(x(j));
 end
-
-
-end
-
 plot(x,uh,'b+',x,u,'r')
 
 
